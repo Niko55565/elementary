cf42556edb5898a3fb5f959a8e7c6048ce39c899
Niko <Kolabaranov15@gmail.com>
2023-10-07 19:08:18 +0700

    1.14 string

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java";
+        System.out.println(idea);
+        idea += " but I am newbie ";
+        System.out.println(idea);
+        int year = 2023;
+        idea += year;
+        System.out.println(idea);
+    }
+}
